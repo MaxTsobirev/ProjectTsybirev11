d33a46287ae05a9f31dd06f649961659da7ed8d7
opilane <opilane@HP-CZC6297B8Z>
2022-05-20 13:25:00 +0300

    Add project files.

---
+++ ProjectTsybirev/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ProjectTsybirev
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
